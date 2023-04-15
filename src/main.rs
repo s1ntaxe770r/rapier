6e98c5fa90fd083f5fe9cccf83fbed11b3b1cb05
s1ntaxe770r <cyberdev01@protonmail.com>
2023-04-15 15:13:52 +0100

    actix re-write ✨

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-use rapier::info::ClusterInfo;
+use rapier::{info::ClusterInfo,};
+use rapier::handler::get_region;
+use actix_web::{web, App, HttpServer};
 
-
-
-#[tokio::main] 
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
     let cluster_info = ClusterInfo::new().await;
-    println!("Cluster region: {}", cluster_info.get_cluster_region().await);
-    Ok(())
+    let cfg = rapier::config::ApiConfig::new();
+  
+    log::debug!("Starting rapier on port {}", cfg.server_port);
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(cluster_info.clone()))
+            .service(get_region)
+    }).bind(("0.0.0.0", cfg.server_port))?
+        .run()
+        .await
 }
